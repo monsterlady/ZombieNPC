e469b27b18979d518bcf31446ac1e7908da0b61e
Bestitz <xuruikang6@hotmail.com>
2020-09-25 15:59:26 +0200

    - test

--- Assets/Scripts/KnockWalkBehaviour.cs
+++ Assets/Scripts/KnockWalkBehaviour.cs
@@ -6,6 +6,7 @@ public class KnockWalkBehaviour : StateMachineBehaviour
 {
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        //return Idle state
         animator.SetBool("isBumped",false);
     }
 }
